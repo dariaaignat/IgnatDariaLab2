ea362e1e96b6a64515396c8727351ce187494560
Daria <dariaandreeaignat@gmail.com>
2024-12-08 17:52:57 +0200

    Lab3

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace IgnatDariaLab2.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
+}
